8f361d99d8e99c48db63fa2d273b20129e3d6b63
amosaugusto <amosaugusto@gmail.com>
2022-04-08 12:34:12 +0700

    fix http server

--- server/index.js
+++ server/index.js
@@ -25,12 +25,14 @@
              res.writeHead(200, {"Content-Type": "text/html"});
              res.end(html);
          });
-     }else if(req.url === "/getcars") {
-         const dataPath = path.join(__dirname, '../data', '/cars.json');
-         const fileStream = fs.createReadStream(dataPath, "UTF-8");
-         res.writeHead(200, {"Content-Type": "application/json"});
-         fileStream.pipe(res);
-     }else if(req.url.match("\.css$")){
+     }
+    //  else if(req.url === "/getcars") {
+    //      const dataPath = path.join(__dirname, '../data', '/cars.json');
+    //      const fileStream = fs.createReadStream(dataPath, "UTF-8");
+    //      res.writeHead(200, {"Content-Type": "application/json"});
+    //      fileStream.pipe(res);
+    //  }
+     else if(req.url.match("\.css$")){
         //  console.log('masuk sini====css===');
          const cssPath = path.join(__dirname, '../public', req.url);
          const fileStream = fs.createReadStream(cssPath, "UTF-8");
